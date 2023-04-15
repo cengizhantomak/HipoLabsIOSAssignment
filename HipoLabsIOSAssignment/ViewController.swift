27c0c83a2af644967e33af41cbe5a45ccf1b4ff1
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-04-15 18:09:48 +0300

    Initial Commit

---
+++ HipoLabsIOSAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HipoLabsIOSAssignment
+//
+//  Created by Kerem Tuna Tomak on 15.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
